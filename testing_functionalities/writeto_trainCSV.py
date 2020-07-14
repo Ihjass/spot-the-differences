c3f62c0db38688a2edf785ee50401d7a50ee4f6b
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-15 00:20:33 +0530

    helper module for reading file names and writing into CSV file

--- testing_functionalities/writeto_trainCSV.py
+++ testing_functionalities/writeto_trainCSV.py
@@ -1,15 +1,17 @@
 import get_files as GetFiles
 import csv
-
+# get all file names under the given path
 allFileNames = GetFiles.getListofFiles('D:\\Nivedhithaa\\Sixth-Semester\\mlpackage\\dataset\\')
 row_list = []
+# for each file name, accordingly create a row to be added in CSV
 for i in range(len(allFileNames)):
     print(allFileNames[i])
     total = [allFileNames[i],"image","Swami"]
     row_list.append(total)
 
-
+# on opening 'train.csv', write the row that is needed
 with open('train.csv','w',newline='') as file:
     csv_writer = csv.writer(file)
     csv_writer.writerow(["fileName","Size","Type"])
+    # for multiple rows use 'writerows' instead of 'writerow'
     csv_writer.writerows(row_list)
