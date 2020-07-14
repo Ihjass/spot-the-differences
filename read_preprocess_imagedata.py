24d06bb73f0427b24cde5f8ffb5d70a1ac241ecb
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-14 22:38:50 +0530

    Update read_preprocess_imagedata.py

--- read_preprocess_imagedata.py
+++ read_preprocess_imagedata.py
@@ -1,34 +1,3 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pandas as pd
-#
-#
-# def getSplitData():
-#     train = pd.read_csv('ObjDetTrain.csv')
-#     train.head()
-#     X = train.iloc[:, :-1].values
-#     Y = train.iloc[:, -1].values
-#     x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
-#     return ((x_train, y_train), (x_test, y_test))
-#
-#
-# if __name__ == '__main__':
-#     nRowsRead = 1000
-#     df1 = pd.read_csv('ObjDetTrain.csv', delimiter=',', nrows=nRowsRead)
-#     df1.dataframeName = 'ObjDetTrain.csv'
-#     nRow, nCol = df1.shape
-#     print(f'There are {nRow} rows and {nCol} columns')
-#     df1.head(5)
-#     print(getSplitData())
-
 import pandas as pd
 import cv2 as cvimg
 import numpy as np
@@ -57,7 +26,7 @@ def flatten_images():
 if __name__ == '__main__':
     folder = "dataset/"
     nRowsRead = 1000
-    df1 = pd.read_csv('ObjDetTrain.csv', delimiter=',', nrows=nRowsRead)
+    df1 = pd.read_csv('dataset/ObjDetTrain.csv', delimiter=',', nrows=nRowsRead)
     df1.dataframeName = 'ObjDetTrain.csv'
     nRow, nCol = df1.shape
     print(f'There are {nRow} rows and {nCol} columns')
@@ -65,4 +34,3 @@ if __name__ == '__main__':
     print(getSplitData())
     images=[]                             # list contatining  all images
     print(flatten_images())
-
