981c75b93d6d4ef767c285c6a6224a55d4ea6346
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-14 16:16:39 +0000

    Add files via upload

---
+++ testing_functionalities/extractColumnsCSV.py
@@ -0,0 +1,16 @@
+import csv
+totalrow_list=[]
+with open('dataset/main_dataset.csv', newline='') as csvfile:
+    data = csv.DictReader(csvfile)
+    print("ID Department Name")
+    print("---------------------------------")
+    i=0
+    flag = 0
+    for row in data:
+        full_row = row['name'],row['category'],row['img_paths']
+        print(full_row)
+        totalrow_list.append(full_row)
+with open('ObjDetTrain.csv', 'w', newline='\n') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(["fileName", "Category", "Paths","Class"])
+    csv_writer.writerows(totalrow_list)
