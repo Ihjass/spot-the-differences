b47fb70233e9bbc9640eaa21fba1e741e038e2d8
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-14 22:45:03 +0530

    Update read_preprocess_imagedata.py

--- read_preprocess_imagedata.py
+++ read_preprocess_imagedata.py
@@ -1,10 +1,17 @@
+#for reading csv file
 import pandas as pd
+#cv2 is library for image purpose
 import cv2 as cvimg
+#numpy for mathematic,linear algebraic purposes
 import numpy as np
+#os is imported as we deal with files
 import os
+#pyplot is needed for plotting data
 import matplotlib.pyplot as plt
-import glob
+#splitting train and test data 
+from sklearn.model_selection import train_test_split
 
+#module to split the data and return tuples of train and test data
 def getSplitData():
     train = pd.read_csv('dataset/ObjDetTrain.csv')
     train.head()
@@ -13,24 +20,12 @@ def getSplitData():
     x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
     return ((x_train, y_train), (x_test, y_test))
 
-def flatten_images():
-    for filename in os.listdir(folder):
-        print(filename)
-        img=plt.imread(folder+filename)  # reading image (Folder path and image name )
-        img=np.array(img)                #
-        img=img.flatten()                # Flatten image
-        images.append(img)               # Appending all images in 'images' list
-    return images
-
-
 if __name__ == '__main__':
     folder = "dataset/"
     nRowsRead = 1000
-    df1 = pd.read_csv('dataset/ObjDetTrain.csv', delimiter=',', nrows=nRowsRead)
+    df1 = pd.read_csv('ObjDetTrain.csv', delimiter=',', nrows=nRowsRead)
     df1.dataframeName = 'ObjDetTrain.csv'
     nRow, nCol = df1.shape
     print(f'There are {nRow} rows and {nCol} columns')
     df1.head(5)
     print(getSplitData())
-    images=[]                             # list contatining  all images
-    print(flatten_images())
