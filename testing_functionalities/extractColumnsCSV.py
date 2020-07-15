0b5649059d5e155625e04dbb29b75c89687511e8
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-15 07:15:30 +0530

    update helper module - csv to csv

--- testing_functionalities/extractColumnsCSV.py
+++ testing_functionalities/extractColumnsCSV.py
@@ -1,17 +1,27 @@
+#csv module is needd here for copying specific colmuns to another csv
 import csv
 totalrow_list=[]
-with open('dataset/main_dataset.csv', newline='') as csvfile:
+# copy from train to test - wohtout the class labels
+with open('ObjDetTrain.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
-    print("ID Department Name")
-    print("---------------------------------")
+    print("----------Data----------")
+    print("------------------------")
     i=0
     flag = 0
     for row in data:
-        full_row = row['name'],row['category'],row['img_paths']
-        print(full_row)
-        totalrow_list.append(full_row)
-        
-with open('ObjDetTrain.csv', 'w', newline='\n') as file:
+        i+=1
+        actual_result = row['Class']
+        if(i<=20):
+            continue
+
+        if i>20 and i<150:
+            full_row = row['fileName'],row['Category'],row['Paths']
+            print(full_row)
+            totalrow_list.append(full_row)
+        else:
+            break
+# write into test file 
+with open('ObjDetTest.csv', 'w', newline='\n') as file:
     csv_writer = csv.writer(file)
-    csv_writer.writerow(["fileName", "Category", "Paths","Class"])
+    csv_writer.writerow(["fileName", "Category", "Paths"])
     csv_writer.writerows(totalrow_list)
