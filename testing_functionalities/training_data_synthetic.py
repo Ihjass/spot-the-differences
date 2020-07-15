15188f370eb09e1bf395f23e04e6b66764a1f0c7
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-07-15 13:17:19 +0530

    update helper

--- testing_functionalities/training_data_synthetic.py
+++ testing_functionalities/training_data_synthetic.py
@@ -1,11 +1,16 @@
+#import numpy for any conversions to array
 import numpy as np
+#import os for locating path
 import os
+#for synthesis - import skimage modules
 import skimage.io as io
 import skimage.transform as transform
+#for visualising data import matplotlib modules
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 # %matplotlib inline
-I = io.imread('dataset/book-covers/Art-Photography/0000002.jpg')/255.0
+imag_name=''
+I = io.imread(imag_name)/255.0
 
 # making the foreground synthetic
 front = I.copy()
